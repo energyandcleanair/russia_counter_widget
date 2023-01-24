ab39b5c0a07790d274712eb02a4e32c445472278
Lohit Bisen <lohitkbisen@gmail.com>
2023-01-24 19:39:05 +0530

    code refactor

--- counter_with_bar/counter.js
+++ counter_with_bar/counter.js
@@ -294,19 +294,24 @@ const ui_setup = async(container) => {
     x.style.display = URL_SETTINGS.show_caption ? 'block' : 'none'
 }
 
+const broadCastHeight = ()=> {
+    let message = { 
+        element:'russia_widget_content', 
+        height: document.documentElement.scrollHeight
+    };
+    window.top.postMessage(message, "*");
+}
 const start_counter = async(container)=>{
     URL_SETTINGS = get_url_params();
     await ui_setup(container)
     await pull_data();
 
     //sending the message again once data is loaded
-    let message = { element:'russia_widget_content', height: document.documentElement.scrollHeight};
-	window.top.postMessage(message, "*");
+    broadCastHeight()
 }
 
 // For pages that are using the widget in iframe
-window.addEventListener('load', function() {
-    let message = { element:'russia_widget_content', height: document.documentElement.scrollHeight};
 
-	window.top.postMessage(message, "*");
-});
+window.addEventListener('load', function() { broadCastHeight() });
+
+
